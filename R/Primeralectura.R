36c4b4765ef306aa46c7238421d680012ec971d8
plciani <91813094+plciani@users.noreply.github.com>
2021-11-08 01:52:53 -0300

    Update Primeralectura.R
    Se busca corregir errores

--- R/Primeralectura.R
+++ R/Primeralectura.R
@@ -1,34 +1,7 @@
 library(tidyverse)
 library(rio)
-#AtencionesUrgencia2014 <- read_excel("Data/AtencionesUrgencia2014.xlsx")
-#View(AtencionesUrgencia2014)
-#datosfinales <- AtencionesUrgencia2014
-
 test1=rio::import("Data/AtencionesUrgencia2014.xlsx") %>% 
   rowid_to_column()
-
-#names(datosfinales)
-#view(datosfinales)
-
-#datosfinales2 <- datosfinales %>% 
-  #rowid_to_column()
-
-#datosfinales2 %>% 
-  #ggplot(aes(rowid,`SECCIÓN 1. TOTAL ATENCIONES DE URGENCIA`))+
-  #geom_point()
-
-
-#datosfinales$`TOTAL CAUSAS SISTEMA RESPIRATORIO`[2]
-#max(datosfinales2$`TOTAL CAUSAS SISTEMA RESPIRATORIO`)
-
-#datosfinales2 %>% 
-  #ggplot(aes(rowid,`TOTAL CAUSAS SISTEMA RESPIRATORIO`))+
-  #geom_point()+
-  #labs(title="Evolución por semana de entradas a Urgencia por causas atribuídas al sistema respiratorio, año 2014",
-  #     x='Semanas',y="Total causas",caption="Elaboración propia con datos obtenidos desde Ministerio Salud")+
-  #xlim(0,55)+
-  #ylim(0,180000)
-
 datosfinales2=test1
 ggplot()+
   xlim(0,60)+
@@ -43,13 +16,4 @@ ggplot()+
            parse=TRUE,
            col="#56B4E9")+
   theme_minimal()
-  
-  
-  
-
-
-
-
-
-
-  
+ 
